87b1189dc10c7261497c04e952fc778f55e6ab4a
Emilie Mutoniwabo <edmutoni@ad.unc.edu>
2023-09-19 22:27:43 -0400

    one shot wordle final final final

--- exercises/ex02_one_shot_wordle.py
+++ exercises/ex02_one_shot_wordle.py
@@ -1,4 +1,4 @@
-"""One Shot Wordle"""
+"""One Shot Wordle."""
 __author__ = "730670385"
 
 secret_word: str = "python"
@@ -10,29 +10,26 @@ YELLOW_BOX: str = "\U0001F7E8"
 user_index: int = 0
 secret_index: int = 0
 wordle_boxes: str = ""
-letter_check: bool = False
-secret_indice: int = 0
 
 while len(user_word) != secret_length:
     new_word: str = input(f"That was not {secret_length} letters! Try Again: ")
     user_word = new_word
-
 if user_word == secret_word:
     print("Woo! You got it!")
 else:
     print("Not quite. Play again soon!")
-
 while user_index < secret_length:
     if user_word[user_index] == secret_word[secret_index]:
         wordle_boxes = wordle_boxes + GREEN_BOX
     else:
-        while letter_check == False and secret_indice < secret_length:
+        letter_check: bool = False
+        secret_indice: int = 0
+        while letter_check is False and secret_indice < secret_length:
             if secret_word[secret_indice] == user_word[user_index]:
                 letter_check = True 
             else:
                 secret_indice = secret_indice + 1
-                letter_check = False
-        if letter_check == True:
+        if letter_check is True:
             wordle_boxes = wordle_boxes + YELLOW_BOX
         else:
             wordle_boxes = wordle_boxes + WHITE_BOX
