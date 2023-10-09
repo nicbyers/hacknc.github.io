f4845b6d4d34e52f4b424ed49d4091976fd51ce2
Emilie Mutoniwabo <edmutoni@ad.unc.edu>
2023-10-09 19:17:41 -0400

    list utils

--- exercises/ex04_utils.py
+++ exercises/ex04_utils.py
@@ -1,7 +1,9 @@
-"""Lists Utils Exercise"""
+"""Lists Utils Exercise."""
+____author__ = "730670385"
 
-def all(int_list: list[int], num_input: int ) -> bool:
-    """Returning T or F if all the ints in the list is the same as the int provided"""
+
+def all(int_list: list[int], num_input: int) -> bool:
+    """Returning T or F if all the ints in the list is the same as the int provided."""
     i: int = 0
     if len(int_list) == 0:
         return False
@@ -14,7 +16,7 @@ def all(int_list: list[int], num_input: int ) -> bool:
 
 
 def max(input: list[int]) -> int:
-    """Returning the maxmimum value given a list of integers"""
+    """Returning the maxmimum value given a list of integers."""
     if len(input) == 0:
         raise ValueError("max() arg is an empty List")
     i: int = 1
@@ -29,18 +31,13 @@ def max(input: list[int]) -> int:
 
 
 def is_equal(f_list: list[int], s_list: list[int]) -> bool:
-    """Returning T or F if every element in both lists is the same at the same index"""
+    """Returning T or F if every element in both lists is the same at the same index."""
     i: int = 0
-    if len(f_list) <= len(s_list):
+    if len(f_list) == len(s_list):
         while i < len(f_list):
             if f_list[i] == s_list[i]:
                 i = i + 1
             else:
                 return False
-    else:
-        while i < len(s_list):
-            if f_list[i] == s_list[i]:
-                i = i + 1
-            else:
-                return False
-    return True
+        return True
+    return False
